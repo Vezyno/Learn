1d1eb553d89fa33c61ea49d0eadae7bf222faa9d
Vezyno <nikita.solovey.061606@gmail.com>
2024-10-20 02:50:26 +0300

    123

---
+++ Lesson5_string.py
@@ -0,0 +1,20 @@
+mystring = 'bla bla bla'
+name = 'mr vASya pupkin'
+
+print(name.title())
+print(name.upper())
+print(name.lower())
+
+print('Privet stroka №1\nPrivet stroka №2\n\nPrivet stroka №3')
+print('Messages:\n\tMessage1\n\tMessage2\n\tMessage3\n')
+print('Messages:\n\tMessage1\n\tMessage2\n\tMessage3\nEND')
+print('Lower name: ' + name.lower())
+
+print('--------------------\n\n')
+a = '...... ,dadya vasya  ..........'
+print(a)
+print(a.rstrip())
+print(a.lstrip())
+print(a.strip())
+b = a.strip('.')
+print(b.strip())
