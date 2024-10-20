c6512a523a3deb9c97bfc08b592a0a19b05bb5d3
Vezyno <nikita.solovey.061606@gmail.com>
2024-10-21 01:22:10 +0300

    4

--- Lesson8-Lists.py
+++ Lesson8-Lists.py
@@ -5,15 +5,15 @@ print(sities[-1])
 print(sities[2].title())
 
 sities[2] = 'Tula'
-sities.append('Yalta')
+sities.append('Yalta') #добавить элемент справа
 sities.append('Kursk')
 print(sities)
 
-sities.insert(2,'Feodosiya')
+sities.insert(2,'Feodosiya') #вставить элемент 
 print(sities)
 
 del sities[-1]
-sities.remove('New York')
+sities.remove('New York') #удалить элемент
 print(sities)
 
 delited_city = sities.pop() #вырезать элемент списка
