a1d5427d882d4622ce6b0d30221183280fb9031a
Vezyno <nikita.solovey.061606@gmail.com>
2024-10-22 23:14:32 +0300

    7

--- Lesson11-Functions.py
+++ Lesson11-Functions.py
@@ -21,11 +21,23 @@ for i in range(15):
 print('----------------------------------')
 
 def create_record(name, telephone, adress):
+    """create record"""
     record = {
         'name': name,
         'phone': telephone,
         'adress': adress
     }
+    return record
 
-user1 = create_record('vasya', 894561237, 'pushkina 52')
+user1 = create_record('vasya', '+794561237', 'pushkina 52')
+user2 = create_record('petya', '+795143243', 'bagrationa 45')
 print(user1)
+print(user2)
+
+def give_award(medal, *persons):
+    """Give medal to persons"""
+    for person in persons:
+        print('Tovaricsh ' + person.title() + ' nagrajdaetsya medaliyu ' + medal.title())
+
+give_award('za berlin', 'vasya', 'petya')
+give_award('za London', 'vasya', 'petya', 'nikita', 'misha')
---
+++ Lesson12-MOD.py
@@ -0,0 +1,18 @@
+def aaa():
+    print('aaaa')
+
+
+def bbb():
+    print('bbbb')
+
+
+def ccc():
+    print('cccc')
+
+
+def ddd():
+    print('dddd')
+
+
+def eee():
+    print('eeee')
